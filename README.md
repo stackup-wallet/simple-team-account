b3b031f3ea9e19387ce91b30fe307d3e224f27e3
hazim-j <hazim.jumali@gmail.com>
2024-12-19 14:55:38 +1100

    PoC implementation

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Simple Team Account
 
 This repository is a fork of [eth-infinitism/account-abstraction](https://github.com/eth-infinitism/account-abstraction) for implementing a `SimpleAccount` variant for the [Stackup platform](https://stackup.fi/).
+
+| Contract                                                                           | Address                                      |
+| ---------------------------------------------------------------------------------- | -------------------------------------------- |
+| [`SimpleTeamAccount.sol`](./contracts/samples/SimpleTeamAccount.sol)               | `0x9F19C6a27CEA6b40C031954A01A710714fD750Bc` |
+| [`SimpleTeamAccountFactory.sol`](./contracts/samples/SimpleTeamAccountFactory.sol) | `0xC04aB952581658671D1f6f1Daa8738a0725F0425` |
+
+## Usage
+
+Before being able to run any command, you need to create a `.env` file and set your environment variables. You can
+follow the example in `.env.example`.
+
+Install dependencies:
+
+```shell
+yarn install
+forge install
+```
+
+Compile contracts:
+
+```shell
+yarn compile
+```
+
+Deploy `SimpleTeamAccountFactory`:
+
+```shell
+yarn run deploy
+```
---
+++ contracts/samples/SimpleTeamAccount.sol
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.23;
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+/* solhint-disable reason-string */
+
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "../core/BaseAccount.sol";
+import "../core/Helpers.sol";
+import "./callback/TokenCallbackHandler.sol";
+
+import {LibString} from "solady/utils/LibString.sol";
+import {WebAuthn} from "webauthn-sol/WebAuthn.sol";
+import {Base64} from "openzeppelin-contracts/contracts/utils/Base64.sol";
+
+enum Access {
+    Outsider,
+    Member,
+    Owner
+}
+
+struct Signer {
+    uint256 p256x;
+    uint256 p256y;
+    address ecdsa;
+    Access level;
+}
+
+/**
+ * Minimal team account.
+ *  This is a simple account for teams that require a shared treasury.
+ *  It is lightweight by design and does not support modules or onchain permissions.
+ *  Any access control logic should be computed and signed offchain.
+ *
+ *  This implementation supports 4 signature types:
+ *      1. Owner with WebAuthn
+ *      2. Member with WebAuthn and verified with ECDSA
+ *      3. Owner with ECDSA
+ *      4. Member with ECDSA and verified with ECDSA
+ *
+ * SimpleTeamAccount is originally forked from SimpleAccount.
+ */
+contract SimpleTeamAccount is BaseAccount, TokenCallbackHandler, Initializable {
+    using LibString for string;
+
+    address public verifier;
+    uint256 public signerId;
+    mapping(uint256 id => Signer s) signers;
+
+    IEntryPoint private immutable _entryPoint;
+
+    event SimpleTeamAccountInitialized(IEntryPoint indexed entryPoint, Signer initSigner, address initVerifier);
+
+    /// @inheritdoc BaseAccount
+    function entryPoint() public view virtual override returns (IEntryPoint) {
+        return _entryPoint;
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
+    constructor(IEntryPoint anEntryPoint) {
+        _entryPoint = anEntryPoint;
+        _disableInitializers();
+    }
+
+    /**
+     * execute a transaction (called directly from owner, or by entryPoint)
+     * @param dest destination address to call
+     * @param value the value to pass in this call
+     * @param func the calldata to pass in this call
+     */
+    function execute(address dest, uint256 value, bytes calldata func) external {
+        _requireFromEntryPoint();
+        _call(dest, value, func);
+    }
+
+    /**
+     * execute a sequence of transactions
+     * @dev to reduce gas consumption for trivial case (no value), use a zero-length array to mean zero value
+     * @param dest an array of destination addresses
+     * @param value an array of values to pass to each call. can be zero-length for no-value calls
+     * @param func an array of calldata to pass to each call
+     */
+    function executeBatch(address[] calldata dest, uint256[] calldata value, bytes[] calldata func) external {
+        _requireFromEntryPoint();
+        require(dest.length == func.length && (value.length == 0 || value.length == func.length), "wrong array lengths");
+        if (value.length == 0) {
+            for (uint256 i = 0; i < dest.length; i++) {
+                _call(dest[i], 0, func[i]);
+            }
+        } else {
+            for (uint256 i = 0; i < dest.length; i++) {
+                _call(dest[i], value[i], func[i]);
+            }
+        }
+    }
+
+    /**
+     * @dev The _entryPoint member is immutable, to reduce gas consumption.
+     * @param initSigner the owner (signer) of this account
+     * @param initVerifier An authorized entity for approving member level transactions.
+     */
+    function initialize(Signer calldata initSigner, address initVerifier) public virtual initializer {
+        _initialize(initSigner, initVerifier);
+    }
+
+    function _initialize(Signer calldata initSigner, address initVerifier) internal virtual {
+        verifier = initVerifier;
+        signers[signerId] = initSigner;
+        signerId++;
+        emit SimpleTeamAccountInitialized(_entryPoint, initSigner, initVerifier);
+    }
+
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        virtual
+        override
+        returns (uint256 validationData)
+    {
+        Signer memory signer = signers[uint256(bytes32(userOp.signature[:32]))];
+        require(signer.level != Access.Outsider, "account: unauthorized");
+
+        // Assuming WebAuthn signature.
+        bytes calldata data = userOp.signature[32:];
+        if (signer.ecdsa == address(0)) {
+            if (signer.level == Access.Owner) {
+                return _validateWebAuthnOwner(signer, userOpHash, data);
+            }
+            return _validateWebAuthnMember(signer, userOpHash, data);
+        }
+
+        // Assuming ECDSA signature.
+        if (signer.level == Access.Owner) {
+            return _validateECDSAOwner(signer, userOpHash, data);
+        }
+        return _validateECDSAMember(signer, userOpHash, data);
+    }
+
+    function _validateWebAuthnOwner(Signer memory signer, bytes32 userOpHash, bytes calldata data)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes memory challenge = abi.encode(userOpHash);
+        WebAuthn.WebAuthnAuth memory auth = _webAuthn(challenge, data);
+        return WebAuthn.verify(challenge, true, auth, signer.p256x, signer.p256y)
+            ? SIG_VALIDATION_SUCCESS
+            : SIG_VALIDATION_FAILED;
+    }
+
+    function _validateECDSAOwner(Signer memory signer, bytes32 userOpHash, bytes calldata data)
+        internal
+        pure
+        returns (uint256 validationData)
+    {
+        return signer.ecdsa == ECDSA.recover(MessageHashUtils.toEthSignedMessageHash(userOpHash), data)
+            ? SIG_VALIDATION_SUCCESS
+            : SIG_VALIDATION_FAILED;
+    }
+
+    function _validateWebAuthnMember(Signer memory signer, bytes32 userOpHash, bytes calldata data)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes memory challenge = abi.encode(userOpHash);
+        WebAuthn.WebAuthnAuth memory auth = _webAuthn(challenge, data[65:]);
+
+        return verifier == ECDSA.recover(MessageHashUtils.toEthSignedMessageHash(userOpHash), data[:65])
+            && WebAuthn.verify(challenge, true, auth, signer.p256x, signer.p256y)
+            ? SIG_VALIDATION_SUCCESS
+            : SIG_VALIDATION_FAILED;
+    }
+
+    function _validateECDSAMember(Signer memory signer, bytes32 userOpHash, bytes calldata data)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 hash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        return verifier == ECDSA.recover(hash, data[:65]) && signer.ecdsa == ECDSA.recover(hash, data[65:])
+            ? SIG_VALIDATION_SUCCESS
+            : SIG_VALIDATION_FAILED;
+    }
+
+    function _webAuthn(bytes memory challenge, bytes memory data)
+        internal
+        pure
+        returns (WebAuthn.WebAuthnAuth memory auth)
+    {
+        (
+            bytes memory authenticatorData,
+            string memory clientDataJSONPre,
+            string memory clientDataJSONPost,
+            uint256 challengeIndex,
+            uint256 typeIndex,
+            uint256 r,
+            uint256 s
+        ) = abi.decode(data, (bytes, string, string, uint256, uint256, uint256, uint256));
+        auth = WebAuthn.WebAuthnAuth({
+            authenticatorData: authenticatorData,
+            clientDataJSON: clientDataJSONPre.concat(Base64.encodeURL(challenge)).concat(clientDataJSONPost),
+            challengeIndex: challengeIndex,
+            typeIndex: typeIndex,
+            r: r,
+            s: s
+        });
+    }
+
+    function _call(address target, uint256 value, bytes memory data) internal {
+        (bool success, bytes memory result) = target.call{value: value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+}
